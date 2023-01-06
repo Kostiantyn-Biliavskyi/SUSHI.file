55339a6a8e93b2177c754648092820d07b2b69b3
Kostiantyn-Biliavskyi <gattikostya@gmail.com>
2023-01-06 16:51:35 +0200

    06.01.23. slaider, button

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// ====///////
+
+mainSlider.addEventListener("click", typ);
+
+function typ(e) {
+
+    var y = e.target.dataset.nameSushi;
+
+    switch (y) {
+        case 'fil':
+            alert("lososi");
+            break;
+        case 'krev':
+            alert("krevetki");
+            break;
+    }
+};
+
+
