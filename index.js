79ef8c9781f30670c8207b82e50af5d33eeec181
Kostiantyn-Biliavskyi <gattikostya@gmail.com>
2023-01-07 20:41:42 +0200

    slayider движение фото

--- index.js
+++ index.js
@@ -2,9 +2,7 @@
 mainSlider.addEventListener("click", typ);
 
 function typ(e) {
-
     var y = e.target.dataset.nameSushi;
-
     switch (y) {
         case 'fil':
             alert("lososi");
@@ -12,7 +10,26 @@ function typ(e) {
         case 'krev':
             alert("krevetki");
             break;
+        default:
     }
 };
 
+function movementFotoMain() {
+    let lengthFoto = mainSlider.querySelectorAll('ul>li>img');
+    let widthMainSliderFoto = mainSlider.offsetWidth, sum = widthMainSliderFoto, i = 1;
+    function go() {
+        i++;
+        if (i <= lengthFoto.length) {
+            sum = sum - widthMainSliderFoto;
+            mainSliderUl.style.left = sum + "px";
+        } else {
+            sum = 0;
+            i = 1;
+            mainSliderUl.style.left = sum + "px";
+        }
+        setTimeout(go, 4000);
+    };
+    go();
 
+};
+movementFotoMain();
