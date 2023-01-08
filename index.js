ec7823c3ffa552cdc9ff9911809d081a43b90fce
Kostiantyn-Biliavskyi <gattikostya@gmail.com>
2023-01-08 12:21:59 +0200

    js slider foto main centr

--- index.js
+++ index.js
@@ -13,23 +13,38 @@ function typ(e) {
         default:
     }
 };
-
+// --------------- функция слайдера фото и точки меняют цвет ------------------------
 function movementFotoMain() {
     let lengthFoto = mainSlider.querySelectorAll('ul>li>img');
-    let widthMainSliderFoto = mainSlider.offsetWidth, sum = widthMainSliderFoto, i = 1;
+    let widthMainSliderFoto = mainSlider.offsetWidth;
+    let sum = widthMainSliderFoto, i = -1, ii = -1;
+    let sliderDot = navigationMainSlider1.querySelectorAll('ul>li');
+
     function go() {
         i++;
-        if (i <= lengthFoto.length) {
+        if (i < lengthFoto.length) {
             sum = sum - widthMainSliderFoto;
             mainSliderUl.style.left = sum + "px";
+            sliderDot[i].style.backgroundColor = "#f66206";
         } else {
             sum = 0;
-            i = 1;
+            i = 0;
+            sliderDot[i].style.backgroundColor = "#f66206";
             mainSliderUl.style.left = sum + "px";
         }
         setTimeout(go, 4000);
     };
     go();
 
+    function goDotBlack() {
+        ii++;
+        sliderDot[ii].style.backgroundColor = "#A4ACAD";
+        if (ii == lengthFoto.length - 1) {
+            ii = -1;
+        }
+        setTimeout(goDotBlack, 4000);
+    };
+    setTimeout(goDotBlack, 4000);
 };
 movementFotoMain();
+// ----------------------- функция слайдера фото и точки меняют цвет ------------------
