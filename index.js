2c99f7e7b0b627230ab559c241caa99f2d532d82
Kostiantyn-Biliavskyi <gattikostya@gmail.com>
2023-01-10 18:41:25 +0200

    10.01.23. половина сделана

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 
+var stopSetTimeoutGo, stopGoDotBlack, i = -1, sliderDot;
 mainSlider.addEventListener("click", typ);
 
 function typ(e) {
@@ -14,37 +15,64 @@ function typ(e) {
     }
 };
 // --------------- функция слайдера фото и точки меняют цвет ------------------------
+
 function movementFotoMain() {
     let lengthFoto = mainSlider.querySelectorAll('ul>li>img');
     let widthMainSliderFoto = mainSlider.offsetWidth;
-    let sum = widthMainSliderFoto, i = -1, ii = -1;
-    let sliderDot = navigationMainSlider1.querySelectorAll('ul>li');
-
+    let sum = widthMainSliderFoto;
+    sliderDot = navigationMainSlider.querySelectorAll('ul>li');
+    i = -1;
     function go() {
         i++;
         if (i < lengthFoto.length) {
             sum = sum - widthMainSliderFoto;
             mainSliderUl.style.left = sum + "px";
-            sliderDot[i].style.backgroundColor = "#f66206";
         } else {
             sum = 0;
             i = 0;
-            sliderDot[i].style.backgroundColor = "#f66206";
             mainSliderUl.style.left = sum + "px";
         }
-        setTimeout(go, 4000);
+        stopSetTimeoutGo = setTimeout(go, 4000);
+
+        switch (i) {
+            case 0:
+                sliderDot[4].style.backgroundColor = "#A4ACAD";
+                sliderDot[0].style.backgroundColor = "#f66206";
+                break;
+            case 1:
+                sliderDot[0].style.backgroundColor = "#A4ACAD";
+                sliderDot[1].style.backgroundColor = "#f66206";
+                break;
+
+            case 2:
+                sliderDot[1].style.backgroundColor = "#A4ACAD";
+                sliderDot[2].style.backgroundColor = "#f66206";
+                break;
+            case 3:
+                sliderDot[2].style.backgroundColor = "#A4ACAD";
+                sliderDot[3].style.backgroundColor = "#f66206";
+                break;
+            case 4:
+                sliderDot[3].style.backgroundColor = "#A4ACAD";
+                sliderDot[4].style.backgroundColor = "#f66206";
+                break;
+            default:
+        }
     };
     go();
 
-    function goDotBlack() {
-        ii++;
-        sliderDot[ii].style.backgroundColor = "#A4ACAD";
-        if (ii == lengthFoto.length - 1) {
-            ii = -1;
-        }
-        setTimeout(goDotBlack, 4000);
-    };
-    setTimeout(goDotBlack, 4000);
 };
 movementFotoMain();
+
+mainSlider.addEventListener("mouseover", mouseMainSlider);
+mainSlider.addEventListener("mouseout", mouseLeavesMainSlider);
+function mouseMainSlider(e) {
+    clearTimeout(stopSetTimeoutGo);
+    return 0;
+};
+function mouseLeavesMainSlider() {
+    sliderDot[i].style.backgroundColor = "#A4ACAD";
+    movementFotoMain();
+    return 0;
+};
 // ----------------------- функция слайдера фото и точки меняют цвет ------------------
