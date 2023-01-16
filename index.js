fd41c9155dc58366ae13bd96e75d81effe118b8f
Kostiantyn-Biliavskyi <gattikostya@gmail.com>
2023-01-16 15:29:43 +0200

    16.01. button sliderMini

--- index.js
+++ index.js
@@ -78,30 +78,59 @@ function mouseLeavesMainSlider(e) {
 
 // ----------------------- функция слайдера фото и точки меняют цвет ------------------
 blockSliderMiniButtonLeft.addEventListener("click", sliderMiniLeft);
-blockSliderMiniButtonRigth.addEventListener("click", sliderMiniRigth);
-var sumSliderMini = 0, sliderMiniWidth, widthSliderMiniUl1 = sliderMiniUl1.offsetWidth;
-var sumSliderMiniUl1 = 0;
-function sliderMiniLeft() {
-    let sliderMiniClass = document.querySelector(".sliderMini");
-    sliderMiniWidth = sliderMiniClass.offsetWidth;
-    sumSliderMini = sumSliderMini - sliderMiniWidth;
+blockSliderMiniButtonLeft.addEventListener("mouseover", mi);
+blockSliderMiniButtonLeft.addEventListener("mouseout", ti);
 
-    sumSliderMiniUl1 = widthSliderMiniUl1 - sliderMiniWidth;
-    alert(sumSliderMiniUl1);
-    if (sumSliderMiniUl1 >= sliderMiniWidth) {
+blockSliderMiniButtonRigth.addEventListener("click", sliderMiniRigth);
+blockSliderMiniButtonRigth.addEventListener("mouseover", ma);
+blockSliderMiniButtonRigth.addEventListener("mouseout", to);
+
+var sumSliderMini = 0, sliderMiniWidth, widthSliderMiniUl1 = sliderMiniUl1.offsetWidth;
+var sumSliderMiniUl1 = widthSliderMiniUl1, colorButton1 = "#f08c0a", colorButton2 = "#f3be79";
+var sliderMiniClass = document.querySelector(".sliderMini");
+
+function sliderMiniLeft() {
+    sliderMiniWidth = sliderMiniClass.offsetWidth;
+    if (sumSliderMiniUl1 > sliderMiniWidth) {
+        sumSliderMini = sumSliderMini - sliderMiniWidth;
+        sumSliderMiniUl1 = sumSliderMiniUl1 - sliderMiniWidth;
         sliderMiniUl1.style.left = sumSliderMini + "px";
+        // colorButton1 = "#f08c0a";
+    } else {
+        // colorButton1 = "#f3be79";
     }
 };
+
 function sliderMiniRigth() {
-    // let sliderMiniClass = document.querySelector(".sliderMini");
-    // sliderMiniWidth = sliderMiniClass.offsetWidth;
-
-    // sumSliderMini = sumSliderMini + sliderMiniWidth;
-
-    // sumSliderMiniUl1 = widthSliderMiniUl1 + sumSliderMini;
-
-    // if (sumSliderMiniUl1 > sliderMiniWidth) {
-    //     sliderMiniUl1.style.left = sumSliderMini + "px";
-    // }
-
+    sliderMiniWidth = sliderMiniClass.offsetWidth;
+    if (sumSliderMiniUl1 < widthSliderMiniUl1) {
+        sumSliderMini = sumSliderMini + sliderMiniWidth;
+        sumSliderMiniUl1 = sumSliderMiniUl1 + sliderMiniWidth;
+        sliderMiniUl1.style.left = sumSliderMini + "px";
+        // colorButton1 = "#f3be79";
+    } else {
+        // colorButton1 = "#f08c0a";
+    }
 };
+function mi() {
+    if (sumSliderMiniUl1 > sliderMiniWidth) {
+        colorButton1 = "#f08c0a";
+    } else {
+        colorButton1 = "#f3be79";
+    }
+    blockSliderMiniButtonLeft.style.backgroundColor = colorButton1;
+};
+function ti() {
+    if (sumSliderMiniUl1 < widthSliderMiniUl1) {
+        colorButton1 = "#f3be79";
+    } else {
+        colorButton1 = "#f08c0a";
+    }
+    blockSliderMiniButtonLeft.style.backgroundColor = colorButton2;
+};
+function ma() {
+    blockSliderMiniButtonRigth.style.backgroundColor = colorButton1;
+};
+function to() {
+    blockSliderMiniButtonRigth.style.backgroundColor = colorButton2;
+}
